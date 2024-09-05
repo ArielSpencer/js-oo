07fc018f2c4af53136e76f6c7d491d95774d115b
Ariel Spencer <arielspencer.tech@gmail.com>
2024-09-05 20:28:02 -0300

    feat: implement class structure with default properties and methods

---
+++ User.js
@@ -0,0 +1,20 @@
+class User {
+  constructor(nome, email, nascimento, role, ativo = true) {
+    this.nome = nome
+    this.email = email
+    this.nascimento = nascimento
+    this.role = role || "estudante" // default estudante
+    this.ativo = ativo
+  }
+
+  exibirInfos() {
+    return `${this.nome}, ${this.email}`
+  }
+}
+
+const novoUser = new User('Beatriz', 'b@b.com', '2024-01-04')
+
+console.log(novoUser);
+console.log(novoUser.exibirInfos());
+
+console.log(User.prototype.isPrototypeOf(novoUser));
