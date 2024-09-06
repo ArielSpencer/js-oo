1ed75aa6faa00355123f1ff8b94293e9ae8fb1b3
Ariel Spencer <arielspencer.tech@gmail.com>
2024-09-05 22:30:10 -0300

    feat: add method to Admin class for creating courses

--- Admin.js
+++ Admin.js
@@ -4,9 +4,13 @@ class Admin extends User {
   constructor(nome, email, nascimento, role = "admin", ativo = "true") {
     super(nome, email, nascimento, role, ativo)
   }
+
+  criarCurso(nomeCurso, qtdVagas) {
+    return `curso ${nomeCurso} criado com ${qtdVagas} vagas.`
+  }
 }
 
 const novoAdmin = new Admin("Elara", "e@e.com", "2024-01-05")
 
 console.log(novoAdmin);
-console.log(novoAdmin.exibirInfos());
+console.log(novoAdmin.criarCurso('ADS', 35));
