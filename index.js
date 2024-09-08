789f2ea2816319c44eb36cdc0da60c24dbdc941a
Ariel Spencer <arielspencer.tech@gmail.com>
2024-09-07 22:43:37 -0300

    feat: add name validation in setter and test invalid name assignment in index.js

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ import Admin from "./Admin.js";
 import Docente from "./Docente.js";
 
 const novoUser = new User("Flavia", "f@f.com", "2024-01-01");
-// console.log(novoUser.exibirInfos());
 console.log(novoUser.exibirInfos());
+novoUser.nome = ""
+console.log(novoUser.nome);
 
 // novoUser.#nome = "Mariana";
 // console.log(novoUser.exibirInfos());
 // console.log(novoUser.#nome);
 
-const novoAdmin = new Admin("Elara", "e@e.com", "2024-01-05")
+// const novoAdmin = new Admin("Elara", "e@e.com", "2024-01-05")
 
-console.log(novoAdmin.nome);
+// console.log(novoAdmin.nome);
 // console.log(novoAdmin.exibeNome());
